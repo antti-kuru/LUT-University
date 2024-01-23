3d29a636a78f0d628f1b313ff447c606590aa3d1
Antti Kuru <128390200+antti-kuru@users.noreply.github.com>
2024-01-23 18:56:13 +0200

    Create exercise5.sql

---
+++ Basics of database systems/week2/exercise5.sql
@@ -0,0 +1,15 @@
+INSERT INTO Player VALUES(
+31,
+'Emil',
+'Ruusuvuori',
+'FIN',
+'02/04/1999');
+
+
+INSERT INTO Ranking VALUES(
+31,
+0,
+31,
+'W: 0 - L: 0',
+(SELECT playerid FROM Player WHERE playerid = 31));
+
