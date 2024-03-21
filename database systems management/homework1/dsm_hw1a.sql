adf96f672c209e8e69c9cc1232f37e2d4672f8db
Antti Kuru <128390200+antti-kuru@users.noreply.github.com>
2024-03-21 10:24:52 +0200

    Create dsm_hw1a.sql

---
+++ database systems management/homework1/dsm_hw1a.sql
@@ -0,0 +1,12 @@
+SELECT 
+    p1.n AS name, 
+    p1.i AS item_1, 
+    p1.d AS purchase_date_1, 
+    p2.i AS item_2, 
+    p2.d AS purchase_date_2 
+    
+FROM purchases p1
+JOIN purchases p2 ON p1.n = p2.n 
+    AND p1.d + interval '1 month' = p2.d
+ORDER BY 
+    name;
